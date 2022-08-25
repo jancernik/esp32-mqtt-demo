8d0cbee12e4ba96e9aefbdc45b8608b730766043
Jan Cernik <jancernik12@gmail.com>
2022-08-25 19:20:02 -0300

    Color picker test

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import './style/style.scss';
-import * as mqtt from "mqtt/dist/mqtt"
+import * as mqtt from 'mqtt/dist/mqtt';
+import ColorPicker from 'simple-color-picker';
+import hexRgb from 'hex-rgb';
+
+let color = 'rgb(0, 0, 0)';
+let isCoolDown = false;
 
 const options = {
-  clientId: 'ueyrghgfgf43e5beouwesfwefveghvuer',
-  username: 'jan',
-  password: '1234'
+  clientId: 'u232309rfj32w0e4fv346t23wervyauer',
 };
 
-const client = mqtt.connect('wss://broker.hivemq.com:8884/mqtt', options);
+const client = mqtt.connect('ws://broker.hivemq.com:8000/mqtt', options);
 
 client.on('connect', () => {
   console.log('Connected!');
@@ -17,17 +20,54 @@ client.on('error', (error) => {
   console.log('Error:', error);
 });
 
-client.on('message', function(topic, message) {
+client.on('message', function (topic, message) {
   handleMessage(topic, String.fromCharCode.apply(null, message));
 });
-
-document.getElementById('slider1').addEventListener('change', (e) => {
-  client.publish('jan/esp32/v2', e.target.value);
-  console.log('jan/esp32/v2', e.target.value);
-})
 
 client.subscribe('jan/esp32/v1');
 
 const handleMessage = (topic, message) => {
   console.log(topic, message);
-}
+};
+
+const sendColor = () => {
+  if (!isCoolDown) {
+    isCoolDown = true;
+    client.publish('redes/esp32/rgb', color);
+    console.log('redes/esp32/rgb', color);
+    setTimeout(() => (isCoolDown = false), 300);
+  }
+};
+window.addEventListener('mouseup', () => {
+  sendColor();
+});
+
+window.addEventListener(
+  'touchend',
+  () => {
+    sendColor();
+  },
+  false
+);
+
+window.addEventListener(
+  'touchcancel',
+  () => {
+    sendColor();
+  },
+  false
+);
+
+var colorPicker = new ColorPicker({
+  el: document.body,
+  color: '#123456',
+  background: '#656565',
+  width: '400',
+  height: '400',
+});
+
+colorPicker.onChange((getHexString) => {
+  const { red, green, blue } = hexRgb(getHexString);
+  color = `rgb(${red}, ${green}, ${blue})`;
+  document.body.style.background = color;
+});
